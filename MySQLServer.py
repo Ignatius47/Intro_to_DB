405363a2b4ba76cd50e4bc1312a9bb4207a60aa5
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-07-26 18:04:09 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,16 +5,33 @@ USE alx_book_store;
 import mysql.connector
 from mysql.connector import Error
 
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Red!Fox27#explos"
+        )
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+    except Error as e:
+        print(f"Error creating database: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
 def create_connection():
     try:
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
             password="Red!Fox27#explos",
-            database="library_db"
+            database="alx_book_store"
         )
         if connection.is_connected():
-            print("Connected to the database")
+            print("Connected to the database 'alx_book_store'")
             return connection
     except Error as e:
         print(f"Error: {e}")
@@ -99,3 +116,5 @@ def Order_details(order_detail_id, order_id, book_id, quantity):
         finally:
             cursor.close()
             connection.close()
+
+create_database()
