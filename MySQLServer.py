5afddf66baba93f8399d2c60e10168688686d92b
Ignatius47 <ignatiusx47@gmail.com>
2024-07-26 17:22:57 +0300

    Initial commit

---
+++ MySQLServer.py
@@ -0,0 +1,57 @@
+import mysql.connector
+
+def create_connection():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Red!Fox27#explos",
+        database="library_db"
+    )
+    if connection.is_connected():
+        print("Connected to the database")
+        return connection
+    
+def Books(title, author_id, price, publication_date):
+    connection = create_connection()
+    cursor = connection.cursor()
+    book_query = "INSERT INTO Books (title, author_id, price, publication_date) VALUES (%s, %s, %s, %s)"
+    values = ("The ALX champ", "$20", "12/08/2024")
+    print(book_query, values)
+    cursor.close()
+    connection.close()
+
+def Authors(author_id, author_name):
+    connection = create_connection()
+    cursor = connection.cursor()
+    author_query = "INSERT INTO Authors (author_id, author_name) (VALUES %s, %s)"
+    values = ("Ignatius")
+    print(author_query, values)
+    cursor.close()
+    connection.close()
+
+def Customers(customer_id, customer_name, email, address):
+    connection = create_connection()
+    cursor = connection.cursor()
+    customer_query = "INSERT INTO Customers (customer_id, customer_name, email, address) (VALUES %s, %s, %s, %s)"
+    values = ("Dickens", "dick@gmail.com", "FC2435")
+    print(customer_query, values)
+    cursor.close()
+    connection.close()
+
+def Orders(order_id, customer_id, order_date):
+    connection = create_connection()
+    cursor = connection.cursor()
+    order_query = "INSERT INTO Orders (order_id, customer_id, order_date) (VALUES %s, %s, %s)"
+    values = ("12/08/2024")
+    print(order_query, values)
+    cursor.close()
+    connection.close()
+
+def Order_details(order_detail_id, order_id, book_id, quantity):
+    connection = create_connection()
+    cursor = connection.cursor()
+    details_query = "INSERT INTO Order_details (order_detail_id, order_id, book_id, quantity) (VALUES %s, %s, %s, %s)"
+    values = ("500kg")
+    print(details_query, values)
+    cursor.close()
+    connection.close() 
---
+++ alx_book_store.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Books (
+    book_id PRIMARY KEY,
+    title VARCHAR(130),
+    author_id FOREIGN KEY REFERENCING Authors TABLE,
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id FOREIGN KEY REFERENCING Customers TABLE,
+    order_date DATE
+)
+
+CREATE TABLE Order_Details (
+    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id FOREIGN KEY REFERENCING Orders TABLE,
+    book_id FOREIGN KEY REFERENCING Books TABLE,
+    quantity DOUBLE
+)
