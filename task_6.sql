f5dc1369eb8658169e2ad92348014c73a900f923
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-07-26 19:27:24 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -5,5 +5,5 @@ INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES ( 3, Obed Ehoneah, eobed@sandtech.com, 125 Happiness  Ave.);
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES ( 3, Nehemial Kamolu, 126 Happiness  Ave.);
+VALUES ( 3, Nehemial Kamolu, nkamolu@sandtech.com, 126 Happiness  Ave.);
 
